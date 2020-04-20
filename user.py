8376dc99a0d547f6fa8680b73e06253dc0a2b742
ENAIKA <naikaesther5@gmail.com>
2020-04-20 20:55:24 +0300

    add class methods to check if user exixts

--- user.py
+++ user.py
@@ -23,3 +23,33 @@ class User:
         '''
 
         User.users_list.remove(self)
+
+    @classmethod
+    def find_user(cls,firstname):
+        '''
+        Method that takes in firstname and returns a user that matches the firstname.
+
+        Args:
+            firstname: name to be searched
+        Returns :
+           user that matches the firstname
+        '''
+
+        for user in cls.users_list:
+            if user.firstname == firstname:
+                return user
+
+    @classmethod
+    def user_exist(cls,firstname):
+        '''
+        Method that checks if a user exists from the users list.
+        Args:
+            firstname: name to e searched
+        Returns :
+            Boolean: dependent on the existance of the user
+        '''
+        for user in cls.users_list:
+            if user.firstname == firstname:
+                    return True
+
+        return False
