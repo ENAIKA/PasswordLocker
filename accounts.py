fba963bd9d3bf967851397f23e08a5c44738e404
ENAIKA <naikaesther5@gmail.com>
2020-04-21 00:20:55 +0300

    add @classmethods-find_account(), account_exist() and display_accounts

--- accounts.py
+++ accounts.py
@@ -25,4 +25,42 @@ class Credentials:
 
         Credentials.accounts_list.remove(self)
 
+    @classmethod
+    def find_account(cls,accountName):
+        '''
+        Method that takes in account name and returns an account that matches the accountName.
+
+        Args:
+            accountName: name to be searched
+        Returns :
+           account that matches the accountName
+        '''
+
+        for account in cls.accounts_list:
+            if account.accountName == accountName:
+                return account
+
+    @classmethod
+    def account_exist(cls,accountName):
+        '''
+        Method that checks if an account exists from the accounts list.
+        Args:
+            accountName: name to be searched
+        Returns :
+            Boolean: dependent on the existance of the account
+        '''
+        for account in cls.accounts_list:
+            if account.accountName == accountName:
+                    return True
+
+        return False
+
+    @classmethod
+    def display_accounts(cls):
+        '''
+        method that returns the accounts list
+        '''
+        return cls.accounts_list
+
+
     
