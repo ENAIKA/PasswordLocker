523a5d1a22b1b2fdca88de9f546c93ae0ae4d804
ENAIKA <naikaesther5@gmail.com>
2020-04-20 23:59:27 +0300

    create class Credentials, init(), save_account(), delete_account() methods

---
+++ accounts.py
@@ -0,0 +1,28 @@
+class Credentials:
+    '''
+    Class that generates new instances of Credentials
+    '''
+    accounts_list=[] #empty accounts list
+
+    def __init__(self, accountName, username, password):
+        self.accountName=accountName
+        self.username=username
+        self.password=password
+
+    def save_account(self):
+
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
+
+        Credentials.accounts_list.append(self)
+
+    def delete_account(self):
+
+        '''
+        delete_account method deletes a saved account
+        '''
+
+        Credentials.accounts_list.remove(self)
+
+    
