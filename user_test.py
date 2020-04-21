c814cb8454a106b5506005f77fec3c43bb88b206
ENAIKA <naikaesther5@gmail.com>
2020-04-21 13:45:21 +0300

    modify comment

--- user_test.py
+++ user_test.py
@@ -29,7 +29,7 @@ class TestUser(unittest.TestCase):
 
     def test_save_user(self):
         '''
-        test_save_contact test case to test if the user object is saved into
+        test_save_user test case to test if the user object is saved into
          the users list
         '''
         self.new_user.save_user() # saving the new user
